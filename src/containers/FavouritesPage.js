9f3032290de56da1262e614da45df7cd56525053
Alexoid1 <alex_zam@hotmail.es>
2021-03-04 21:28:08 -0500

    fix es lint

--- src/containers/FavouritesPage.js
+++ src/containers/FavouritesPage.js
@@ -1,15 +1,13 @@
 import FavouriteSlider from '../components/FavouriteSlider';
 import TestsContainer from '../components/TestsContainer';
-import NotFound from '../containers/NotFound';
 import './FavouritesPage.css';
 
 const FavouritesPage = () => (
   <div className="compCont3" id="website_favourites">
     <h1 className="titleMoto">FAVOURITES MODELS</h1>
-   
-      <FavouriteSlider />
-    
-    
+
+    <FavouriteSlider />
+
     <h1 className="titleMoto">TESTS RIDES</h1>
     <TestsContainer />
 
